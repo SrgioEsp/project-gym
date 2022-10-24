90aaef22c10f9c5fb1a772ce5b757804f64628fa
Seerch18 <sergioespejo02@gmail.com>
2022-10-24 21:40:27 +0200

    create button welcome_screen

--- src/components/WelcomeScreen/WelcomeScreen.js
+++ src/components/WelcomeScreen/WelcomeScreen.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { Button } from 'react-bootstrap';
+
 const WelcomeScreen = (props) => {
-	return <div>WelcomeScreen</div>;
+	return (
+		<div>
+			WelcomeScreen
+			<div className='d-grid gap-2'>
+				<Button href='#' variant='primary' size='lg'>
+					Entrar
+				</Button>
+			</div>
+		</div>
+	);
 };
 
 WelcomeScreen.propTypes = {};
