858b63ab01d196c9e48dd6cf193645a5ec3e15e0
Seerch18 <sergioespejo02@gmail.com>
2022-12-01 13:52:03 +0100

    point out sessions in calendar

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import { Calendar as ReactCalendar } from 'react-calendar';
 
 import 'react-calendar/dist/Calendar.css';
 import { Col, Row } from 'react-bootstrap';
+import { AppContext } from '../../contexts/AppContext';
+import { mapWeekDays } from '../../utils';
 
 const Calendar = ({ value, onChange, setShowModal }) => {
+	const { sessions } = useContext(AppContext);
 	return (
 		<ReactCalendar
 			onChange={onChange}
@@ -14,6 +17,19 @@ const Calendar = ({ value, onChange, setShowModal }) => {
 				setShowModal(true);
 			}}
 			className='calendar'
+			tileClassName={({ date }) => {
+				if (sessions && sessions.length !== 0) {
+					let isActive = false;
+					sessions.forEach((session) => {
+						session.days.weekdays.forEach((weekday) => {
+							if (weekday.day === mapWeekDays[date.getDay()]) {
+								isActive = true;
+							}
+						});
+					});
+					return isActive && 'sessions-calendar-active';
+				}
+			}}
 		/>
 	);
 };
