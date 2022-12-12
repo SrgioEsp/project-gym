4fa923adffd159c4c174ed66cee36886e2eee06d
Seerch18 <sergioespejo02@gmail.com>
2022-12-12 18:15:20 +0100

    url render api

--- src/api/urls.js
+++ src/api/urls.js
@@ -1,8 +1,9 @@
 const urlBase = 'http://localhost:3001';
 const urlBaseApi = 'http://localhost:3002/api';
+const urlRenderApi = 'https://project-gym-server.onrender.com/api';
 
-export const urlTrainees = `${urlBaseApi}/trainees`;
-export const urlTrainee = `${urlBaseApi}/trainee`;
-export const urlUsers = `${urlBaseApi}/users`;
-export const urlSessions = `${urlBaseApi}/sessions`;
-export const urlSession = `${urlBaseApi}/session`;
+export const urlTrainees = `${urlRenderApi}/trainees`;
+export const urlTrainee = `${urlRenderApi}/trainee`;
+export const urlUsers = `${urlRenderApi}/users`;
+export const urlSessions = `${urlRenderApi}/sessions`;
+export const urlSession = `${urlRenderApi}/session`;
